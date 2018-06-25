cb9b1da1b24f670adb3cc9ac2228f1ac10b88db2
Muharrizk Abilaksana <ai.mo7g@gmail.com>
2018-06-25 12:33:11 +0700

    revert main.js back

--- src/main.js
+++ src/main.js
@@ -8,20 +8,13 @@ if(record == null){
 console.log(record);
 var fromGameover, bgmPlaying = false;
 
-document.addEventListener("deviceready", function(){
-	setTimeOut(function(){
-		navigator.splashscreen.hide();
-	}, 5000, false);
-});
-(function(){
-	CarFreeDay.game = new Phaser.Game(720, 1280, Phaser.AUTO, '', null, false, false);
+CarFreeDay.game = new Phaser.Game(720, 1280, Phaser.AUTO, '', null, false, false);
 
-	CarFreeDay.game.state.add('Boot', CarFreeDay.Boot);
-	CarFreeDay.game.state.add('Preload', CarFreeDay.Preload);
+CarFreeDay.game.state.add('Boot', CarFreeDay.Boot);
+CarFreeDay.game.state.add('Preload', CarFreeDay.Preload);
 
-	CarFreeDay.game.state.add('Title', CarFreeDay.Title);
-	CarFreeDay.game.state.add('Game', CarFreeDay.Game);
-	//CarFreeDay.game.state.add('Score', CarFreeDay.Score);
+CarFreeDay.game.state.add('Title', CarFreeDay.Title);
+CarFreeDay.game.state.add('Game', CarFreeDay.Game);
+//CarFreeDay.game.state.add('Score', CarFreeDay.Score);
 
-	CarFreeDay.game.state.start('Boot');
-})();
+CarFreeDay.game.state.start('Boot');
